bae899808848c76ab071428617d09e951a7fe952
Candra Tran <candratran@gmail.com>
2017-07-22 13:21:16 -0700

    can now load file when you drag file into editor!

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 
+
 // const electron = require('electron')
 const {app, BrowserWindow} = require('electron')
 //same as: 
@@ -9,6 +11,22 @@ app.on('ready', () => {
   // mainWindow.loadURL("http://google.com")
   // __dirname = var that is always defined
   const file = 'file://' + __dirname + '/index.html'
-  
   mainWindow.loadURL(file)
+
+  // let's send information from this process to another process
+  // want to build a fewature so user can drag file to window, it will open
+  
+  //  want to make sure chrome doesn't get in the way?
+
+  // Webcontents has all these properties such as "open", which open the dev tools
+  // "on('will-navigate') => to navigate from the page
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    e.preventDefault();
+    console.log(url);
+
+    const realPath = url.slice(7);
+
+    // send has channel, and args
+    mainWindow.webContents.send('open-file', realPath);
+  })
 })
--- renderer.js
+++ renderer.js
@@ -1,10 +1,27 @@
 const loader = require('monaco-loader')
+const { ipcRenderer} = require('electron')
+const fs = require('fs')
 
 loader().then((monaco) => {
   const div = document.getElementById('container')
 
   let editor = monaco.editor.create(div, {
     // line above: create an editor IN the div
-    
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
   })
+
+  ipcRenderer.on('open-file', (e, url) => {
+    console.log(url)
+    //syncronously read the file until it's read
+    // read the file it's received. encoding buffer, this treats it like text
+    // data is the actual contents of index.js when you drag it.
+    const data = fs.readFileSync(url, 'utf-8');
+    editor.setModel(monaco.editor.createModel(data, 'javascript'));
+  })
+
+  ipcRenderer.on('')
+
+
 })
